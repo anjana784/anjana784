ccb9ff91177cb0ba7213cef06f5a5628463fdcff
anjana784 <hello@anjana784.dev>
2025-12-26 06:08:42 +0530

    added scroll controlls

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,23 @@
 "use client";
 import { Experience } from "@/components/experience";
-import { OrbitControls, PerspectiveCamera } from "@react-three/drei";
+import { PerspectiveCamera, ScrollControls } from "@react-three/drei";
 import { Canvas } from "@react-three/fiber";
 
 const HomePage = () => {
   return (
-    <div className="w-dvw h-dvh">
+    <div className="w-dvw h-dvh no-scrollbar">
       <Canvas>
-        <PerspectiveCamera makeDefault position={[0, 0, 5]} />
-        <OrbitControls />
-        <Experience />
+        <ScrollControls
+          pages={3}
+          style={{
+            scrollbarWidth: "none", // Firefox
+            msOverflowStyle: "none", // IE / old Edge
+          }}
+        >
+          <PerspectiveCamera makeDefault position={[0, 0, 5]} />
+          {/* <OrbitControls /> */}
+          <Experience />
+        </ScrollControls>
       </Canvas>
     </div>
   );
--- components/experience.tsx
+++ components/experience.tsx
@@ -1,17 +1,24 @@
-import { useThree } from "@react-three/fiber";
-import { useEffect } from "react";
+"use client";
+import * as THREE from "three";
+import { useFrame } from "@react-three/fiber";
 import { Space } from "./space";
+import { useScroll } from "@react-three/drei";
 
 export const Experience = () => {
-  const state = useThree();
+  const scroll = useScroll();
 
-  useEffect(() => {
-    console.log(state.camera.position);
-  }, [state]);
+  const startZ = 5;
+  const endZ = -5;
 
-  return (
-    <>
-      <Space />
-    </>
-  );
+  useFrame((state) => {
+    // scroll.offset is 0 → 1
+    const progress = scroll.curve(0, 1);
+
+    // Move camera forward along Z
+    state.camera.position.z = THREE.MathUtils.lerp(startZ, endZ, scroll.offset);
+
+    console.log(progress);
+  });
+
+  return <Space />;
 };
